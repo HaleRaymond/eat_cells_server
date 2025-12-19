a82d2aedf5895120c1f543f7618b289fb096a432
HaleRaymond <156083992+HaleRaymond@users.noreply.github.com>
2025-12-19 10:07:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const WebSocket = require("ws");
 /* ---------------- CONFIG ---------------- */
 
 const PORT = process.env.PORT || 8080;
-const TICK_RATE = 20;                 // ticks per second
+const TICK_RATE = 10;                 // ticks per second
 const WORLD_SIZE = 6000;              // square world, center (0,0)
 const BASE_SPEED = 550;               // base movement speed
 const SPLIT_SPEED = 400;             // speed of split pieces
